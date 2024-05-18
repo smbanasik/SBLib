e32b304522ca0c34538e59351a111a21309ced60
smbanasik <smbanasik@gmail.com>
2024-05-17 22:51:16 -0500

    Added some standard type names for integers

--- types.hpp
+++ types.hpp
@@ -1,6 +1,18 @@
 #ifndef SB_LIB_TYPES_HPP
 #define SB_LIB_TYPES_HPP
 
+// TODO: have macros that probe the size of primitive integers and
+// defines these accordingly
+
+typedef signed char int8;
+typedef short int16;
+typedef long int32;
+typedef long long int64;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
 
 
 
