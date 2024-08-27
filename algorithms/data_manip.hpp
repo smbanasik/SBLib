318124c40a557f1a082e118ceef3ba729d722a0f
smbanasik <smbanasik@gmail.com>
2024-08-27 12:06:22 -0500

    Added swap function

--- algorithms/data_manip.hpp
+++ algorithms/data_manip.hpp
@@ -5,6 +5,7 @@
 
 namespace SB_LIB {
 
+    // TODO: redo this to utilize three pointers (end target) rather than mlen length
     template<typename T>
     void ll_copy(T* addr_one, T* addr_two, mlen length) {
         for (mlen idx = 0; idx < length; idx++) {
@@ -12,11 +13,19 @@ namespace SB_LIB {
         }
     }
 
+    // TODO: does this do what we want?
     template<typename ForwardIterator>
     void copy(ForwardIterator first, ForwardIterator last, ForwardIterator target) {
         for (; first != last; ++first, ++target) {
             *target = *first;
         }
+    }
+    
+    template<typename T>
+    void swap(T& one, T& two) {
+        T temp = one;
+        one = two;
+        two = one;
     }
 
 }
