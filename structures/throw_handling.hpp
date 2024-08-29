5aab40a6cddfb6da576ffd61ba663e7b9aeed324
smbanasik <smbanasik@gmail.com>
2024-08-29 16:52:46 -0500

    Plan for throw handling

---
+++ structures/throw_handling.hpp
@@ -0,0 +1,10 @@
+#ifndef SB_LIB_THROW_HANDLING_HPP
+#define SB_LIB_THROW_HANDLING_HPP
+namespace SB_LIB {
+    
+    // TODO: on an error message, create a char array with the error message, line, and file
+    // __FILE__ and __LINE__ macros should be made use of here
+    // Define a macro that, given a type, will produce an exception with the types message, file, and line
+
+}
+#endif
