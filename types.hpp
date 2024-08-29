57a55a08bed2daec2a9c5d2465928b4b8e0a91f0
smbanasik <smbanasik@gmail.com>
2024-08-29 17:00:01 -0500

    Cleaned up tpyes

--- types.hpp
+++ types.hpp
@@ -30,10 +30,8 @@ typedef unsigned short uint16;
 typedef unsigned long uint32;
 
 #ifdef _WIN64
-typedef uint64 mlen;
 #define SB_LIB_PTR_SIZE 8
 #else
-typedef uint32 mlen;
 #define SB_LIB_PTR_SIZE 4
 #endif
 #endif
@@ -53,18 +51,18 @@ typedef unsigned long uint32;
 #endif
 
 #if __SIZEOF_POINTER__ == 8
-typedef uint64 mlen;
 #define SB_LIB_PTR_SIZE 8
 #else
-typedef uint32 mlen;
 #define SB_LIB_PTR_SIZE 4
 #endif
 #endif
 #endif
 
 #ifdef SB_LIB_PTR_SIZE == 8
+typedef uint64 mlen;
 typedef int64 ptr_diff;
 #else
+typedef uint32 mlen;
 typedef int32 ptr_diff; 
 #endif
 
